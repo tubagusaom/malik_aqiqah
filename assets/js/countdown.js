07361587a06de750b94c7f530bbc73854201013c
tubagusaom <tubagus.aom.swk@gmail.com>
2023-10-13 01:12:36 +0700

    mergeserver

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -22,6 +22,6 @@ document.getElementById("demo").innerHTML = days + " Hari " + hours + " Jam " +m
 // If the count down is finished, write some text
 if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "BERLANGSUNG";
     }
 }, 1000);
