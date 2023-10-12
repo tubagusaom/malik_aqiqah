5140800e6b19a63f3b91febd386533d1604ffdfb
tubagusaom <tubagus.aom.swk@gmail.com>
2023-10-13 03:48:13 +0700

    mergenew

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -2,9 +2,13 @@
 // var tanggalAwal = new Date("13 October 2023 19:00:00").getTime();
 
 document.getElementById("demo").replaceChildren();
+// document.getElementById("demo").reset();
+// cache.reset();
+// cache.delete();
+// localStorage.clear();
 
 var tanggalAwal = new Date("13 October 2023 02:42:00").getTime();
-var tanggalAkhir  = new Date("13 October 2023 03:40:00").getTime();
+var tanggalAkhir  = new Date("13 October 2023 03:05:00").getTime();
 
 var xxx =  new Date("13 October 2023 02:26:00").getTime();
 
